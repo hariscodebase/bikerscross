0a14633f1ec5d9da8f6dd1c3f2e0b8ba3be73ac7
Hari <user@DESKTOP-65AVN6U>
2022-06-01 11:43:04 -0400

    Adding assignment files

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = process.env.port || 3000; //set a port number
+
+app.set("views", path.join(__dirname, "views")); //set express views 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public"))); //set path for static files
+
+var siteTitle = "Bikers Cross";
+
+//routes
+app.get("/", (req, res) => {
+    res.render("home", { title: siteTitle, headerText: "Meet new and passionate bikers nearby."})
+});
+
+app.get("/pricing", (req, res) => {
+    res.render("pricing", { title: siteTitle, headerText: "Choose the plan the best suits your needs."});
+})
+
+app.get("/gallery", (req, res) => {
+    res.render("gallery", { title: siteTitle, headerText: "Here are our recent events"});
+})
+
+
+//setup server listen
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+})
